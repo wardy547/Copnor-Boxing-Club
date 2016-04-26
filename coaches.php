973ad13b937cf48899b5ac41a18ea28dd9701b1e
Ryan Ward <wardy547@Gmail.com>
2016-04-26 15:46:05 +0100

    coaches.php changes

--- coaches.php
+++ coaches.php
@@ -2,22 +2,37 @@
 
   <section class="container">
     <div class="row coach">
-      <div class="col-4 coach__image">
+      <div class="col-3 coach__image">
         <img src="./images/photo-2.jpg">
       </div>
-      <div class="col-8 coach__content">
+      <div class="col-3 coach__content">
         <h2 class="coach__name">Bob</h2>
-        <p class="coach__bio">My name is Bob, i approve this message</p>
+        <p class="coach__bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id nulla imperdiet justo feugiat pretium vel venenatis arcu. Sed semper felis eget elit fringilla fringilla. </p>
+      </div>
+     <div class="col-3 coach__image">
+        <img src="./images/photo-2.jpg">
+      </div>
+      <div class="col-3 coach__content">
+        <h2 class="coach__name">Bob</h2>
+        <p class="coach__bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id nulla imperdiet justo feugiat pretium vel venenatis arcu. Sed semper felis eget elit fringilla fringilla.</p>
       </div>
     </div>
     <div class="row coach">
-      <div class="col-4 coach__image">
+      <div class="col-3 coach__image">
         <img src="./images/photo-2.jpg">
       </div>
-      <div class="col-8 coach__content">
+      <div class="col-3 coach__content">
         <h2 class="coach__name">Bob</h2>
-        <p class="coach__bio">My name is Bob, i approve this message</p>
+        <p class="coach__bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id nulla imperdiet justo feugiat pretium vel venenatis arcu. Sed semper felis eget elit fringilla fringilla. </p>
       </div>
+     <div class="col-3 coach__image">
+        <img src="./images/photo-2.jpg">
+      </div>
+      <div class="col-3 coach__content">
+        <h2 class="coach__name">Bob</h2>
+        <p class="coach__bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id nulla imperdiet justo feugiat pretium vel venenatis arcu. Sed semper felis eget elit fringilla fringilla.</p>
+      </div>
+      
     </div>
   </section>
 
