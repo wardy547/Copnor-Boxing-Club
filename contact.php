9e53e8f522819c2b3aaee69cbf1c4119888cd5c5
Ryan Ward <wardy547@Gmail.com>
2016-04-28 13:55:51 +0100

    finalizing my contact formm css

--- contact.php
+++ contact.php
@@ -1,5 +1,7 @@
 <?php require 'header.php' ?>
-
+<section></section>
+<div class="form">
+<h1>Contact Form </h1>
 <form action="form.php" method="post">
     Your name<br>
     <input type="text" name="cf_name"><br>
@@ -9,6 +11,8 @@
     <textarea name="cf_message">
     </textarea>
     
-    <input type="submit" value="Send">
-    <input type="reset" value="Clear">
+    <input class="button" type="submit" value="Send">
+    <input class="button" type="reset" value="Clear">
+    </div>
+ </section>
 </form>
